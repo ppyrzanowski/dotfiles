ab8097173ed1bbcdc6f532bfb2605a15cb735182
Patrik Pyrzanowski <p.pyrzano.s6@gmail.com>
2022-05-05 20:12:18 +0200

    Default configuration

---
+++ init.lua
@@ -0,0 +1,4 @@
+require "user.options" 		-- Entrypoint ./lua/ is implied and file type .lua is impled
+require "user.keymaps"		-- Key bindings and remappings
+require "user.plugins"		-- Plugin configuration
+require "user.colorscheme"
---
+++ lua/user/options.lua
@@ -0,0 +1,21 @@
+vim.opt.backup = false					-- creates a backup file
+vim.opt.clipboard = "unnamedplus"			-- allows neovim to access the system clipboard
+vim.opt.cmdheight = 2					-- more space in the neovim command line for displaying messages
+vim.opt.conceallevel = 0				-- makes `` visisble in markdown files
+vim.opt.fileencoding = "utf-8"
+vim.opt.ignorecase = true				-- ignore case in seach patterns
+
+vim.opt.showtabline = 2					-- always show tab
+vim.opt.smartindent = true
+
+
+vim.opt.termguicolors = true
+
+
+
+
+
+vim.opt.number = true
+
+
+
