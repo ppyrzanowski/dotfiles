fbceec326a0e33cdcef546359d51fa6293d59fda
Patrik Pyrzanowski <p.pyrzano.s6@gmail.com>
2022-05-06 03:31:46 +0200

    Add custom colors to git status

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,3 +5,11 @@ if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
+-- Custom color changes
+
+-- #39FF14
+-- #06c258
+vim.cmd[[ highlight GitSignsAdd guibg=nil guifg=#06c258 ]]
+vim.cmd[[ highlight GitSignsChange guifg=#F7A400 ]]
+vim.cmd[[ highlight GitSignsDelete guifg=#8E1600 ]]
